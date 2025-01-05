c90f580bbc6958ef5005947853bb16067d472b7e
John Carmack <johncarmack@me.com>
2025-01-05 13:10:48 -0800

    Add Tauri updater with signing keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import { useEffect } from "react";
 import "./App.css";
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
 
 function App() {
   useEffect(() => {
-    const update = async () => {
-      await import("./update");
+    const checkUpdate = async () => {
+      const update = await check();
+      if (update) {
+        console.log(
+          `found update ${update.version} from ${update.date} with notes ${update.body}`
+        );
+        let downloaded = 0;
+        let contentLength = 0;
+        // alternatively we could also call update.download() and update.install() separately
+        await update.downloadAndInstall((event) => {
+          switch (event.event) {
+            case "Started":
+              contentLength = event.data.contentLength ?? 0;
+              console.log(
+                `started downloading ${event.data.contentLength} bytes`
+              );
+              break;
+            case "Progress":
+              downloaded += event.data.chunkLength;
+              console.log(`downloaded ${downloaded} from ${contentLength}`);
+              break;
+            case "Finished":
+              console.log("download finished");
+              break;
+          }
+        });
+
+        console.log("update installed");
+        await relaunch();
+      }
     };
-    update();
+    checkUpdate();
   }, []);
   return <>app</>;
 }
