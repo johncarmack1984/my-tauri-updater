fbd684e6a9908dffb3b91234201b2ddcc4eb533e
John Carmack <johncarmack@me.com>
2025-06-01 13:14:36 -0700

    Refine workflows and update matrix configs

---
+++ .scripts/get-release-channel.cjs
@@ -0,0 +1,18 @@
+// @ts-check
+
+const fn = () => {
+  const path = require("node:path");
+  const fs = require("node:fs");
+
+  const loc = path.resolve(__dirname, "../.changes/pre.json");
+  const data = fs.readFileSync(loc, { encoding: "utf-8" });
+  const obj = JSON.parse(data);
+  const out = obj?.tag ?? "stable";
+
+  console.log(out.toString());
+  return out.toString();
+};
+
+const script = ({ ...args }) => fn();
+
+module.exports = script;
--- .scripts/set-msi-version.ts
+++ .scripts/set-msi-version.ts
@@ -1,9 +1,9 @@
-import version from "./get-msi-version";
-import setVersion from "./set-version";
+// import version from "./get-msi-version";
+// import setVersion from "./set-version";
 
-const main = (newVersion: string) => {
-  setVersion(newVersion, true);
-  console.log(`MSI version set to ${version}`);
-};
+// const main = (newVersion: string) => {
+//   setVersion(newVersion, true);
+//   console.log(`MSI version set to ${version}`);
+// };
 
-main(version);
+// main(version);
