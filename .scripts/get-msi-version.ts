d708e4b5d4adea807de6ffe2c53b1ae27acbc422
John Carmack <johncarmack@me.com>
2025-05-31 18:38:05 -0700

    Improve Windows versioning and update workflows

--- .scripts/get-msi-version.ts
+++ .scripts/get-msi-version.ts
@@ -37,18 +37,13 @@ const getReleaseChannel = (preRelease?: string): [number, number] => {
 
 const getMsiVersion = (version: string): string => {
   const [mainVersion, preRelease] = version.split("-");
-  const msiIteration = getReleaseChannel(preRelease).join("");
+  const [preC, preV] = getReleaseChannel(preRelease);
   const [major, minor, patch] = mainVersion
     .split(".")
     .map((n) => Number.parseInt(n, 10));
   const releaseNumber = getReleaseNumber();
 
-  const msiVersion = Math.min(
-    major * 10000 + minor * 100 + patch * 10 + (releaseNumber % 10),
-    65535
-  );
-
-  return `${String(msiVersion)}-${String(msiIteration)}`;
+  return `${major}.${minor}.${patch}-${releaseNumber}`;
 };
 
 const version = getMsiVersion(currentVersion);
